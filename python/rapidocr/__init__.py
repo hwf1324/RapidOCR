97361738261b200fdc690f44158ec24ff7560aca
hwf1324 <1398969445@qq.com>
2025-11-15 02:02:35 +0800

    Fix position calculation error

--- python/rapidocr/__init__.py
+++ python/rapidocr/__init__.py
@@ -106,11 +106,11 @@ class PaddleOCR(ContentRecognizer):
 					words: list[dict] = []
 					for char in line:
 						rect: RectLTWH = RectLTWH.fromCollection(*[Point(point[0], point[1]) for point in char[2]])
-						words.append({"x": rect.left, "y": rect.right, "width": rect.width, "height": rect.height, "text": char[0]})
+						words.append({"x": rect.left, "y": rect.top, "width": rect.width, "height": rect.height, "text": char[0]})
 			else:
 				for box, text in zip(result.boxes.tolist(), result.txts):
 					rect: RectLTWH = RectLTWH.fromCollection(*[Point.fromFloatCollection(*point) for point in box])
-					lines.append([{"x": rect.left, "y": rect.right, "width": rect.width, "height": rect.height, "text": text}])
+					lines.append([{"x": rect.left, "y": rect.top, "width": rect.width, "height": rect.height, "text": text}])
 
 			self._onResult(LinesWordsResult(lines, imageInfo))
 		else:
